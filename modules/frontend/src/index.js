cdc1791917716d004f23cb0a7e71b6c2ca2042d3
Xiaohong Hu <xiaohong@duckduck.io>
2022-12-23 01:50:37 +0800

    update frontend image to latest

--- modules/frontend/src/index.js
+++ modules/frontend/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import ReactDOM from "react-dom";
 import ReactDOM from "react-dom/client"
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -9,7 +8,6 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  //document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
